285a4a8c58834b4a3d2d08ab9e136b9747bb420b
Robert Vagner <robertvsilva0@gmail.com>
2025-02-06 17:20:19 -0300

    alterações solicitadas 06-02

--- login/auth-guard.js
+++
@@ -1,5 +0,0 @@
-supabase.auth.getSession().then(({ data }) => {
-    if (!data.session) {
-        window.location.href = "login.html";
-    }
-});
--- promptPergunta.js
+++ promptPergunta.js
@@ -11,14 +11,14 @@ document.addEventListener("DOMContentLoaded", function() {
         const respostaAluno = respostaAlunoInput.value;
 
         // Construímos a resposta combinando a pergunta e a resposta do aluno
-        const respostaCompleta = `Verifique o percentual de acerto da seguinte resposta : ${respostaAluno} para a questão: ${pergunta}, e explique o motivo da porcentagem e coloque referências bibliográficas.`;
+        const respostaCompleta = `Considere a seguinte questão: ${pergunta}.Verifique o percentual de acerto da seguinte resposta: ${respostaAluno}, e explique o motivo da porcentagem e coloque referências bibliográficas.`;
 
         // Exibimos a resposta combinada no textarea
         respostaTextarea.value = respostaCompleta;
 
         // Limpa os campos de pergunta e resposta do aluno
-        perguntaInput.value = "";
-        respostaAlunoInput.value = "";
+        //perguntaInput.value = "";
+        //respostaAlunoInput.value = "";
 
         // Coloca o foco de volta no campo de pergunta
         perguntaInput.focus();
